9c2e7ff5b7e4f8772de47e36d08fa0921f260dab
febristical <febritical@gmail.com>
2023-09-23 00:43:11 +0900

    Push testing

---
+++ 01_prototype/target/debug/deps/prototype-48740334373da50a.d
@@ -0,0 +1,5 @@
+C:\Users\knh19\OneDrive\문서\HNU_2023_Capstone\prototype\target\debug\deps\prototype-48740334373da50a.rmeta: src\main.rs
+
+C:\Users\knh19\OneDrive\문서\HNU_2023_Capstone\prototype\target\debug\deps\prototype-48740334373da50a.d: src\main.rs
+
+src\main.rs:
---
+++ 01_prototype/target/debug/deps/prototype-9d470904392eda4f.d
@@ -0,0 +1,5 @@
+C:\Users\knh19\OneDrive\문서\HNU_2023_Capstone\prototype\target\debug\deps\prototype-9d470904392eda4f.rmeta: src\main.rs
+
+C:\Users\knh19\OneDrive\문서\HNU_2023_Capstone\prototype\target\debug\deps\prototype-9d470904392eda4f.d: src\main.rs
+
+src\main.rs:
