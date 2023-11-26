642c8ed6277df7b85a66662670c185355a662133
argkays <knh19954@gmail.com>
2023-11-26 22:05:00 +0900

    adfdfasdf

--- 01_prototype/target/release/prototype.d
+++
@@ -1,1 +0,0 @@
-/Users/tokki/Documents/Hnu_2023_Capstone/01_prototype/target/release/prototype: /Users/tokki/Documents/Hnu_2023_Capstone/01_prototype/src/main.rs
--- 02_second/src/gui.rs
+++
@@ -1,5 +0,0 @@
-use iced::{
-    widget,
-    widget::{slider, container, button, column, row, text, image as imageviewer},
-    Alignment, Element, Sandbox, Settings, theme::Radio
-};
