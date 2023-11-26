c5b9e3794a8b42f7b58b9f9c2b689ff8340cb23d
argkays <knh19954@gmail.com>
2023-11-26 22:04:25 +0900

    dsafasd

--- 01_prototype/target/release/prototype.d
+++ 01_prototype/target/release/prototype.d
@@ -1,1 +1,1 @@
-C:\Users\knh19\OneDrive\ -\ usk.ac.kr\문서\HNU_2023_Capstone\01_prototype\target\release\prototype.exe: C:\Users\knh19\OneDrive\ -\ usk.ac.kr\문서\HNU_2023_Capstone\01_prototype\src\main.rs
+/Users/tokki/Documents/Hnu_2023_Capstone/01_prototype/target/release/prototype: /Users/tokki/Documents/Hnu_2023_Capstone/01_prototype/src/main.rs
